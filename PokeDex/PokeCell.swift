22d4244f71cb5859f8b3600b6826bf8de1a6f4d2
dahuggin <dahuggin1@gmail.com>
2017-03-29 23:14:01 -0500

    Completed app

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Daniel Huggins on 3/28/17.
+//  Copyright © 2017 Daniel Huggins. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        
+    }
+    
+}
